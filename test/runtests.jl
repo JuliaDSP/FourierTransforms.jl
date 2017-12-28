3e0c95aacc5c3bad9ae1858f16ba3d9476e8a723
Yingbo Ma <mayingbo5@gmail.com>
2017-12-27 20:01:18 -0800

    Add basic test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
-using DFT
-using Base.Test
+import DFT, FFTW
+import DFT: fft_kernel_sizes
+using Test
 
-# write your own tests here
-@test 1 == 2
+# kernel test
+@testset "FFT kernel codelet" begin
+    for i in fft_kernel_sizes
+        data = rand(i)
+        @test DFT.fft(data) ≈ FFTW.fft(data)
+    end
+end
